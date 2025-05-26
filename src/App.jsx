491d123b66767aa1f3606fd82019ac2290fb31fb
ninda shavina <ninda23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:19 +0700

    penjualan sudah oke by ninda

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./Pages/Dashboard";
+import SalesManagement from "./pages/SalesManagement";
 
 export function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+         <Route path="/penjualan" element={<SalesManagement />} />
       </Route>
     </Routes>
   );
