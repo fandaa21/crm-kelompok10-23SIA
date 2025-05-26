f2da444179fe9f1c48c17b144efdf975d8d88bb0
Radityo Alsandi <Radityo23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:50 +0700

    customer by tyo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./Pages/Dashboard";
+import CustomerManagement from "./pages/CustomerManagement";
 
 export function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/customer" element={<CustomerManagement />} />
       </Route>
     </Routes>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,6 +15,7 @@ const menuItems = [
   { name: 'Dashboard', icon: <LayoutDashboard />, path: '/' },
   { name: 'Produk', icon: <Box />, path: '/produk' },
   { name: 'Laporan', icon: <BarChart2 />, path: '/laporan' },
+  { name: 'Users', icon: <Users />, path: '/customer' },
 ]
 
 const accountItems = [
