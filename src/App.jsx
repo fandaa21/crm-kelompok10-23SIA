54ae5f70c633d31fb4e3b80419b9c8aabd864ccb
ninda <ninda23si@mahasiswa.pcr.ac.id>
2025-06-02 15:05:30 +0700

    Menambahkan halaman login dan regist by ninda

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./Pages/Dashboard";
 import SalesManagement from "./pages/SalesManagement";
+import Login from "./pages/Signin"; 
+import Signup from "./pages/Signup";
 
 export function App() {
   return (
     <Routes>
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
          <Route path="/penjualan" element={<SalesManagement />} />
